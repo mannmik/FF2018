e8cc138aae7f420c4c05cc649af3b9e86cf3330b
MM <ognerdyjock@gmail.com>
2018-07-24 15:50:14 -0700

    added User attributes

--- User/models.py
+++ User/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    userId = models.AutoField(primary_key = True)
+    usrEmail = models.TextField()
+    usrPassword = models.CharField(max_length = 25)
+    usrName = models.CharField(max_length = 25)
