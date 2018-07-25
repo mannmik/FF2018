cc5dc7dc618dbba8d2ba21947e0d8b53fe917e9d
MM <ognerdyjock@gmail.com>
2018-07-25 16:24:03 -0700

    end of day 7/25/18

--- UserAccount/views.py
+++ UserAccount/views.py
@@ -7,6 +7,8 @@ from .models import UserAccount
 
 def LogIn(request):
     if request.method == 'POST':
+        # TODO on sign up is to make all emails lowercase 
+        # check here with a lowercase email from the post
         if request.POST['email']:
             usr = UserAccount.objects.get(usrEmail = request.POST['email'])
             return render(request, 'success.html')
