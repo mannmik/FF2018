19732e728da61137d94329aee5b81df8eb6aa444
MM <ognerdyjock@gmail.com>
2018-07-26 16:26:13 -0700

    began api-service file and end of day 7/26/18.

---
+++ FF2018/api_services.py
@@ -0,0 +1,14 @@
+import json
+
+'''
+This file will handle our api services 
+
+'''
+
+def get_nfl_teams():
+    # need the url of ffn we are going to
+    # set up our parameters
+    # load the request
+    # load json 
+    # to dict?
+    # retrun the dict
