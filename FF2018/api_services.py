e0071ffceae37bbc988cb0d51ca82afbdf35ccb0
MM <ognerdyjock@gmail.com>
2018-07-26 16:27:38 -0700

    end of day 7/26/2018

--- FF2018/api_services.py
+++ FF2018/api_services.py
@@ -1,4 +1,6 @@
 import json
+import requests
+
 
 '''
 This file will handle our api services 
@@ -8,6 +10,7 @@ This file will handle our api services
 def get_nfl_teams():
     # need the url of ffn we are going to
     # set up our parameters
+    # r = requests.get(url, params=params)
     # load the request
     # load json 
     # to dict?
